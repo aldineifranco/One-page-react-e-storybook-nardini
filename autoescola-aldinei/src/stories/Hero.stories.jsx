0eb4125e545fbccc6f60b927047836afe34711cc
Aldinei <93042673+aldineifranco@users.noreply.github.com>
2022-08-30 14:38:08 -0300

    linha abaixo do título

--- autoescola-aldinei/src/stories/Hero.stories.jsx
+++ autoescola-aldinei/src/stories/Hero.stories.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import BgHighwayImage from "./assets/banner-full.jpg";
 import BgCarImage from "./assets/car-image.jpg";
+import Heading from "../components/Heading";
 
 export default {
   title: "Hero",
@@ -18,9 +19,12 @@ export const usage = () => (
 
 export const widhList = () => (
   <Hero image={BgCarImage}>
-    <h1>
-      Ganhe sua <strong>liberdade</strong> para ir e vir
-    </h1>
+    <Heading>
+      <h1>
+        Ganhe sua <strong>liberdade</strong>
+        <br /> para ir e vir
+      </h1>
+    </Heading>
     <ul>
       <li>is simply dummy text of the printing and typesetting industry... </li>
       <li>is simply dummy text of the printing and typesetting industry. </li>
