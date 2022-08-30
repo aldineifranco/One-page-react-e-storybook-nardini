df99a11e06262a2baab445afb939c4a2bd3fd57a
Aldinei <93042673+aldineifranco@users.noreply.github.com>
2022-08-30 09:28:21 -0300

    configurando diferentes tamanhos de tela.

--- autoescola-aldinei/.storybook/preview.js
+++ autoescola-aldinei/.storybook/preview.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { addParameters } from "@storybook/react";
 
 import GlobalStyle from "../src/components/GlobalStyle";
 
@@ -15,3 +16,49 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   layout: "fullscreen",
 };
+
+// No projeto estamos trabalhando com o conceito de mobile first
+
+const viewports = {
+  extraSmall: {
+    name: "Portrait phone (default)",
+    styles: {
+      width: "360px",
+      height: "640px",
+    },
+  },
+  small: {
+    name: "Landscape phone (sm)",
+    styles: {
+      width: "640px",
+      height: "360px",
+    },
+  },
+  medium: {
+    name: "Tablet (md)",
+    styles: {
+      width: "768px",
+      height: "1024px",
+    },
+  },
+  large: {
+    name: "Desktop (lg)",
+    styles: {
+      width: "1024px",
+      height: "1366px",
+    },
+  },
+  extraLarge: {
+    name: "Large Desktop (xl)",
+    styles: {
+      width: "1280px",
+      height: "800px",
+    },
+  },
+};
+
+addParameters({
+  viewport: {
+    viewports,
+  },
+});
