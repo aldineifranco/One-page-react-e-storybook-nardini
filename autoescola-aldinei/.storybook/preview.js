bd02d9b3bc2dfd4317670638622124566c4824e5
Aldinei <93042673+aldineifranco@users.noreply.github.com>
2022-08-30 09:14:35 -0300

    styled Component, editorConfig, BreakPoint,

--- autoescola-aldinei/.storybook/preview.js
+++ autoescola-aldinei/.storybook/preview.js
@@ -1,9 +1,17 @@
+import React from "react";
+
+import GlobalStyle from "../src/components/GlobalStyle";
+
+export const decorators = [
+  (StoryFn) => (
+    <>
+      <GlobalStyle />
+      <StoryFn />
+    </>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+  layout: "fullscreen",
+};
---
+++ autoescola-aldinei/src/stories/Hero.stories.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Hero from "../components/Hero";
+import BgHighwayImage from "./assets/banner-full.jpg";
+import BgCarImage from "./assets/car-image.jpg";
+
+export default {
+  title: "Hero",
+  component: Hero,
+};
+
+export const usage = () => (
+  <Hero title="Ganhe sua liberdade para ir e vir" image={BgHighwayImage}>
+    <p>A auto escola lider em aprovação.</p>
+  </Hero>
+);
+
+export const widhList = () => (
+  <Hero title="Ganhe sua liberdade para ir e vir" image={BgCarImage}>
+    <ul>
+      <li>is simply dummy text of the printing and typesetting industry... </li>
+      <li>is simply dummy text of the printing and typesetting industry. </li>
+      <li>is simply dummy text of the printing and typesetting industry. </li>
+    </ul>
+  </Hero>
+);
